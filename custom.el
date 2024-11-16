49f101cfd972bb17652e915f46f7f1838046db2a
Chris Lowis <chris.lowis@gmail.com>
2024-11-16 12:00:41 +0000

    Prettier mode is a safe local variable

--- custom.el
+++ custom.el
@@ -6,7 +6,8 @@
  '(custom-safe-themes
    '("7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" default))
  '(package-selected-packages
-   '(gptel rbtagger jtsx markdown-mode which-key marginalia orderless vertico-directory vertico consult vterm projectile diff-hl magit doom-themes exec-path-from-shell)))
+   '(gptel rbtagger jtsx markdown-mode which-key marginalia orderless vertico-directory vertico consult vterm projectile diff-hl magit doom-themes exec-path-from-shell))
+ '(safe-local-variable-values '((eval prettier-mode t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
