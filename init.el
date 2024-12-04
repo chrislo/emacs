fa65e3e42da9f2a1a7212e13ec84588dcd99d707
Chris Lowis <chris.lowis@gmail.com>
2024-12-04 17:13:21 +0000

    Always use gfm-mode for markdown
    It's a bit less confusing this way!

--- init.el
+++ init.el
@@ -233,9 +233,8 @@
 (use-package markdown-mode
   :defer t
   :commands (markdown-mode gfm-mode)
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
+  :mode (("\\.md\\'" . gfm-mode)
+         ("\\.markdown\\'" . gfm-mode))
   :init (setq markdown-command "multimarkdown")
   )
 
