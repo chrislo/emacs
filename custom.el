ee1eb19138b9093c33f0f97a2eecf335a8ab72d3
Chris Lowis <chris.lowis@gmail.com>
2025-01-14 11:51:39 +0000

    Add prettier package

--- custom.el
+++ custom.el
@@ -6,14 +6,14 @@
  '(custom-safe-themes
    '("7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" default))
  '(package-selected-packages
-   '(denote cape corfu gptel rbtagger jtsx markdown-mode which-key marginalia orderless vertico-directory vertico consult vterm projectile diff-hl magit doom-themes exec-path-from-shell))
+   '(prettier editorconfig denote cape corfu gptel rbtagger jtsx markdown-mode which-key marginalia orderless vertico-directory vertico consult vterm projectile diff-hl magit doom-themes exec-path-from-shell))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
            (lambda nil
              (shell-command-to-string
               (concat "bundle exec htmlbeautifier -b1 " buffer-file-name)))
            nil t)
-     (eval prettier-mode t))))
+     (eval prettier-mode t)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
